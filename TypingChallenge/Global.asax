1c5a42eca051e49695e1acfb402792a4dccd1d3a
Bozeman, Todd <bozemanwow@gmail.com>
2018-02-06 22:34:45 -0500

    Base Master Commit
    Base Master Commit, Forget to setup very first commit, but not work was done either way

---
+++ TypingChallenge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TypingChallenge.MvcApplication" Language="C#" %>
---
+++ TypingChallenge/Scripts/MyScripts/CountDownHelper.js
@@ -0,0 +1,36 @@
+﻿var intival = 1000;
+var counting = false;
+var timerElem = document.getElementById("Timer");
+var wordsTypedElem = document.getElementById("WordsTyped");
+var userInput = document.getElementById("Panel2");
+function Startcount(secs) {
+
+    
+    if (!counting)
+    {
+        counting = true;
+       
+        
+        countDown(secs);
+       
+    }
+    
+}
+function countDown(secs)
+{
+    var numOfwords = userInput.value.split(" ").length;
+    timerElem.innerHTML = "Timer " + secs + "!" + numOfwords.toString();
+        secs--;
+
+        
+
+        wordsTypedElem.innerHTML = numOfwords + "/" + 100;
+
+        if (secs >= 0)
+            var timer = setTimeout('countDown(' + secs + ')', intival);
+        else {
+
+            counting = false;
+
+        }
+}
