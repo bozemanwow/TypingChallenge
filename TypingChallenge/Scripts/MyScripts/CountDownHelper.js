22fbae1ee24f9671b12f735d854deaa9942de94e
Bozeman, Todd <bozemanwow@gmail.com>
2018-02-09 12:19:08 -0500

    Starting Error Detecting

--- TypingChallenge/Scripts/MyScripts/CountDownHelper.js
+++ TypingChallenge/Scripts/MyScripts/CountDownHelper.js
@@ -1,41 +1,49 @@
-﻿var intival = 1000;
+﻿// Magic Variables
+var intival = 1000;
 var lessThanTen = 10;
-var counting = false;
+var testingInput = false;
+var wrappingup = false;
+var timeLength = 59;
+
+
 var displayTextCharCount = 0;
 var displayTextWordCount = 0;
-var timeLength = 59;
+
+
+// Elements
 var timerElem = document.getElementById("Timer");
 var wordsTypedElem = document.getElementById("WordsTyped");
 var displayText = document.getElementById("Panel1");
 var userInput = document.getElementById("Panel2");
-var wordsPercent = document.getElementById("WordsPercent");
-
+var wordsPercent = document.getElementById("WPM");
+var debug = document.getElementById("Debug");
 
 document.onkeypress = keyDetection;
 
 function keyDetection(event)
 {
-    if (!counting)
-    {
-        displayTextCharCount = displayText.innerText.length;
-        displayTextWordCount = displayText.innerText.split(" ").length;
-        userInput.removeAttribute("disabled");
-        userInput.focus();  
-        setCurrentTypedCount();
-        setUiPanel3(0, 0, 0);
-        
-    }
-    else
-    {
-        userInput.focus();  
-        setCurrentTypedCount();
+    if (!wrappingup) {
+        if (!testingInput) {
+            testingInput = true;
+            displayTextCharCount = displayText.innerText.length;
+            displayTextWordCount = displayText.innerText.split(" ").length;
+            userInput.removeAttribute("disabled");
+            userInput.focus();
+            setCurrentTypedCount();
+            setUiPanel3(0, 0, 0);
+
+        }
+        else {
+            userInput.focus();
+            setCurrentTypedCount();
+        }
     }
 }
 
 function setUiPanel3(secs,min,hours)
 {
    
-    if (counting) {
+    if (testingInput) {
         timerElem.innerHTML = convertToTimerstring(secs, min, hours);
         secs++;
         if (secs > timeLength) {
@@ -53,7 +61,11 @@ function setUiPanel3(secs,min,hours)
         setTimeout('setUiPanel3(' + secs.toString() + ',' + min.toString() + ',' + hours.toString() + ')', intival);
     }
     else
-        setTestResults(secs, min, hours);
+    {
+        wrappingup = true;
+        setTimeout('setTestResults(' + secs.toString() + ',' + min.toString() + ',' + hours.toString() + ')', 10);
+    }
+        
   
 }
 
@@ -63,32 +75,42 @@ function convertToTimerstring(secs, min, hours)
     var minStr = min < lessThanTen ? "0" + min.toString() : min.toString();
     var hoursStr = hours < lessThanTen ? "0" + hours.toString() : hours.toString();
 
-    return hoursStr + ":" + minStr + ":" + secStr;
+    return "Time:"+hoursStr + ":" + minStr + ":" + secStr;
 }
 
 function setCurrentTypedCount()
 {
-    counting = userInput.value.length < displayTextCharCount;
+    testingInput = userInput.value.length < displayTextCharCount - 1;
+    wordsTypedElem.innerText = userInput.value.split(" ").length + " Words Typed ";
    
-    if (!counting)
+    if (!testingInput)
     {
-       
-        userInput.setAttribute("disabled", "disabled");        
+        setTimeout("disableInput()", 1);
+          
     }
-   
-       // wordsTypedElem.innerText = userInput.value.split(" ").length + " Words Typed ";
+ 
 }
-
+function disableInput()
+{
+    userInput.setAttribute("disabled", "disabled");     
+}
 function setTestResults(secs, min, hours)
 {
-    var totalTimemins = (secs + (min * 60) + (hours * 60 * 60)) / timeLength;
-    wordsTypedElem.innerText = (userInput.value.length / totalTimemins).toString() + " Words per minute ";
-   
+    var totalTimemins = (secs + min * 60 + hours * 60 * 60) / 60;
+    var errorOffset = findErrors();
+    debug.innerHTML = totalTimemins.toString() + " " + userInput.value.split(" ").length.toString();
+    wordsPercent.innerText = (Math.floor(userInput.value.split(" ").length / totalTimemins)).toString() + " WPM ";
+    
 }
-
+function findErrors()
+{
+    return -1;
+}
 function resetTest()
 {
-    counting = false;
+    testingInput = false;
+    wrappingup = false;
     userInput.value = "";
     timerElem.innerHTML = "Start typing sample";
+    wordsTypedElem.innerText = debug.innerHTML = wordsPercent.innerText = "";
 }
